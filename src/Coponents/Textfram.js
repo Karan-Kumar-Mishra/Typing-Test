f1f2a40dbcdc546b9560e1038248f15b6277ea1f
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2024-08-30 18:17:56 +0530

    adding the animation coponent

--- src/Coponents/Textfram.js
+++ src/Coponents/Textfram.js
@@ -1,17 +1,19 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+
+import {  useState } from "react";
 import Notification from "./Notification";
 import Timer from "./Timer";
+import Backgound from "./Backgound";
 export default function Textfram() {
   const [numberOfword, setnumberOfword] = useState(0);
   const [textmode, settextmode] = useState(false);
   const [incuurectWord, setincuurectWord] = useState(0);
   const [notify, setnotify] = useState(true);
   const [rendermsg, setrendermsg] = useState(false);
-  const [timerRender,settimerRender ]= useState(false)
+  const [timerRender, settimerRender] = useState(false);
   const [word, setword] = useState(
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Proin ac ligula auctor, scelerisque est nec, vehicula magna. Integer ut diam id quam sagittis dignissim. Suspendisse potenti. Ut id nisi a quam fermentum laoreet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Proin ac ligula auctor, scelerisque est nec, vehicula magna."
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Proin ac ligula auctor, scelerisque est nec, vehicula magna. Integer ut diam id quam sagittis"
   );
+
 
   function find_WPM(data) {
     let numberOfWords = 0;
@@ -47,16 +49,13 @@ export default function Textfram() {
 
   return (
     <div className="bg-slate-900 fixed z-10 h-full w-screen flex flex-col justify-center items-center">
-     {
-      timerRender ? <Timer/> : <> </>
-     }
-
+      {timerRender ? <Timer /> : <> </>}
+      <Backgound/>
       {rendermsg ? (
         <Notification
           numberOfWord={numberOfword}
           notify={notify}
           incuurectWord={incuurectWord}
-          
         />
       ) : (
         <> </>
@@ -66,19 +65,20 @@ export default function Textfram() {
         disabled={textmode}
         spellCheck={false}
         onChange={(e) => {
-          settimerRender(true)
+          settimerRender(true);
           setTimeout(() => {
             typing(e.target.value);
             setnotify(true);
             setrendermsg(true);
-            settimerRender(false)
+            settimerRender(false);
           }, 60000);
         }}
         type="text"
         id="text"
-        className="h-80 w-[50rem] z-20 focus:outline-none font-sans fixed  bg-transparent text-3xl bg-red-900 border-slate-900  no-resize text-white"
+        
+        className="h-[9rem] w-[50rem] mb-[6rem] typing-cursor z-30 focus:outline-none  font-sans fixed  bg-transparent text-3xl bg-red-900 border-slate-900  no-resize text-white"
       ></textarea>
-      <h1 className=" text-white h-50 w-[50rem] fixed pt-[2px] flex flex-col justify-center  opacity-30 z-10  text-wrap text-3xl">
+      <h1 className="text-white h-50 mb-[6rem] w-[50rem] fixed pt-[2px] flex flex-col justify-center  opacity-40 z-20  text-wrap text-3xl shadow-text">
         {word}
       </h1>
     </div>
